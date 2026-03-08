7a0086ff33da7522efb792f145c048f408638e72
JackDanna <jackdanna78@gmail.com>
2026-03-08 14:44:46 -0400

    Added the dotnet pkg to nix

--- flake.nix
+++ flake.nix
@@ -19,11 +19,19 @@
             allowUnfree = true;
           };
         };
+
+        dotnet-full =
+          with pkgs.dotnetCorePackages;
+          combinePackages [
+            dotnet_8.sdk
+            dotnet_10.sdk
+          ];
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             vscode-fhs
+            dotnet-full
           ];
         };
       }
