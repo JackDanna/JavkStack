f11969373ae4eb402225bbcd2db1a821ed4ad9e0
JackDanna <jackdanna78@gmail.com>
2026-03-08 15:15:50 -0400

    created the default.nix

---
+++ Shared/default.nix
@@ -0,0 +1,44 @@
+let
+  lockFile = builtins.fromJSON (builtins.readFile ../flake.lock);
+  nixpkgsLock = lockFile.nodes.nixpkgs.locked;
+in
+{
+  nixpkgs ? builtins.getFlake "github:${nixpkgsLock.owner}/${nixpkgsLock.repo}/${nixpkgsLock.rev}",
+  system ? "x86_64-linux",
+  pkgs ? import nixpkgs { inherit system; },
+  src ? pkgs.nix-gitignore.gitignoreSource [ ] ./.,
+}:
+# Build the .NET application for containerization
+pkgs.buildDotnetModule {
+  pname = "shared";
+  version = "1.0.0";
+
+  inherit src;
+
+  projectFile = "Shared.fsproj";
+  nugetDeps = ./deps.json;
+
+  packNupkg = true;
+
+  dotnet-sdk = pkgs.dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_10_0;
+
+  # Build configuration
+  buildType = "Release";
+  dotnetFlags = [
+    "--verbosity"
+    "detailed"
+  ];
+
+  # Runtime dependencies
+  runtimeDeps = with pkgs; [
+    openssl
+    zlib
+    icu
+  ];
+
+  meta = with pkgs.lib; {
+    description = "";
+    platforms = platforms.all;
+  };
+}
