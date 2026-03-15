6a069908b7b94c4ff9a2509ffc69125c0683511b
JackDanna <jackdanna78@gmail.com>
2026-03-15 13:24:55 -0400

    Added the subscription to the config

--- CloudInfrastructure/Stack.fs
+++ CloudInfrastructure/Stack.fs
@@ -1,13 +1,23 @@
 module CloudInfrastructure.Stack
 
+open Pulumi
 open Pulumi.FSharp
 open Pulumi.AzureNative.Resources
 
 let resources () =
+    let config = Config "azure-native"
+
+    let provider =
+        Pulumi.AzureNative.Provider(
+            "azure-provider",
+            Pulumi.AzureNative.ProviderArgs(SubscriptionId = input (config.Require "subscriptionId"))
+        )
+
     let resourceGroup =
         ResourceGroup(
             "javkstack-rg",
-            ResourceGroupArgs(Location = input "eastus")
+            ResourceGroupArgs(Location = input "eastus"),
+            CustomResourceOptions(Provider = provider)
         )
 
     dict [ 
