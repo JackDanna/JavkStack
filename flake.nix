0ecfbace06a718b6f621258eaed7a92b02bc45d6
JackDanna <jackdanna78@gmail.com>
2026-03-28 00:08:30 -0400

    Configuered the flake to work with the React Native app

--- flake.nix
+++ flake.nix
@@ -18,9 +18,20 @@
           inherit system;
           config = {
             allowUnfree = true;
+            android_sdk.accept_license = true;
           };
         };
         commands = import ./commands.nix { inherit pkgs; };
+        
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          cmdLineToolsVersion = "13.0";
+          platformVersions = [ "35" ];
+          buildToolsVersions = [ "35.0.0" ];
+          includeEmulator = true;
+          includeSystemImages = true;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "x86_64" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -28,6 +39,10 @@
             ${commands.set.welcome}
             ${commands.set.commandInstructions} 
             . ${commands.set.changeShellPrompt}
+      
+            export ANDROID_HOME=${androidComposition.androidsdk}/libexec/android-sdk
+            export ANDROID_SDK_ROOT=$ANDROID_HOME
+            export LD_LIBRARY_PATH=/run/opengl-driver/lib:${pkgs.libGL}/lib:${pkgs.libGLU}/lib:${pkgs.gtk3}/lib:$LD_LIBRARY_PATH
           '';
           buildInputs = with pkgs; [
             commands
@@ -39,6 +54,12 @@
             (import ./Shared { inherit pkgs system version; }).passthru.dotnet-sdk
             (import ./Shared { inherit pkgs system version; }).passthru.nodejs
             (import ./Shared { inherit pkgs system version; }).passthru.fable
+
+            jdk17
+            androidComposition.androidsdk
+            libGL
+            libGLU
+            gtk3
           ];
         };
       }
